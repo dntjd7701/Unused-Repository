dc8e0c448ee2d15239de6b7debc9c72886320394
dntjd7701 <dntjd7701@gmail.com>
2021-07-09 12:53:58 +0900

    module test

---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,6 @@
+// const dzMath = require("dntjd7701-math");
+const dzMath = require("dntjd7701-math");
+
+console.log(dzMath.sum(1, 2, 3, 5, 5, 6));
+console.log(dzMath.max(1, 2, 3, 5, 5, 6));
+console.log(dzMath.min(1, 2, 3, 5, 5, 6));
