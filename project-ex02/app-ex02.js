e51fd284b8481099c5b58c86653622332480ad9b
dntjd7701 <dntjd7701@gmail.com>
2021-07-09 14:51:43 +0900

    server applicaion 만들기 연습 #1

--- project-ex02/app-ex02.js
+++ project-ex02/app-ex02.js
@@ -1,4 +1,7 @@
-// const dzMath = require("dntjd7701-math");
+/**
+ * dntjd7701-math.js npm 모듈 테스트(모듈 패키지 : 로컬 배포)
+ * npm i 파일경로
+ */
 const dzMath = require("dntjd7701-math");
 
 console.log(dzMath.sum(1, 2, 3, 5, 5, 6));
