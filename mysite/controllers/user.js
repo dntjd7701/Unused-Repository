ee4e0210ffc8be391a4505d181f6a3d25bf0b32a
dntjd7701 <dntjd7701@gmail.com>
2021-07-15 11:01:24 +0900

    mysite #logging 처리 fin

--- mysite/controllers/user.js
+++ mysite/controllers/user.js
@@ -1,4 +1,3 @@
-
 const models = require('../models'); // directory를 지정하면 index.js가 실행된다. 
 
 module.exports = {
@@ -16,7 +15,6 @@ module.exports = {
             password: req.body.password,
             gender: req.body.gender,
         });
-        console.log(result);
         res.redirect('/user/joinsuccess');
     },
     login: function(req,res){
@@ -48,12 +46,6 @@ module.exports = {
         res.redirect("/");
     },
     update: async function(req,res, next){
-        // 본인은 authUser를 사용했으나, 새롭게 찾아서 하는 방법도 있다. 
-        // try {
-        //     res.render('user/update');
-        // } catch (error) {
-        //     next(error);
-        // }
         try {
             const user = await models.User.findOne({
                 attributes: ['no', 'email', 'name', 'gender'],
@@ -67,24 +59,6 @@ module.exports = {
         }
     },
     _update: async function(req, res, next){
-    //     try {
-    //      const result = await models.User.update({
-    //             name: req.body.name,
-    //             email: req.body.email,
-    //             password: req.body.password || req.session.authUser.password,
-    //             gender: req.body.gender
-    //     },{
-    //         where: {
-    //             no: req.session.authUser.no
-    //         }
-    //      });
-    //     req.session.destroy();
-    //     res.redirect("/user/login");
-    // } catch (error) {
-    //     next(error); //error를 화면에 출력 
-    // }
-    // }
-
     try {
         const {[req.body.password == '' ? 'password' : '']: remove, ...updateObject} = req.body;
         await models.User.update(updateObject, {
@@ -92,19 +66,53 @@ module.exports = {
                 no: req.session.authUser.no    
             }
         });
-        console.log(updateObject);
         req.session.authUser.name=updateObject.name;
-
         res.redirect("/user/update");
     } catch(e) {
         next(e);
     }    
 }
-
-
-
-
 }
+
+
+
+
+
+
 /**
  *  원래는 next와 try catch문을 사용해야한다. route쪽의 error handler로 들어가게된다.
  */
+
+
+/*
+_update: async function(req, res, next){
+        try {
+         const result = await models.User.update({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password || req.session.authUser.password,
+                gender: req.body.gender
+        },{
+            where: {
+                no: req.session.authUser.no
+            }
+         });
+        req.session.destroy();
+        res.redirect("/user/login");
+    } catch (error) {
+        next(error); //error를 화면에 출력 
+    }
+    }
+}
+*/
+
+/*
+update: async function(req,res, next){
+        본인은 authUser를 사용했으나, 새롭게 찾아서 하는 방법도 있다. 
+        try {
+            res.render('user/update');
+        } catch (error) {
+            next(error);
+        }
+       },
+*/
