560b670c095cf4f1ccdc6f35a4cdc8810db69f80
dntjd7701 <dntjd7701@gmail.com>
2021-07-16 14:40:41 +0900

    mysite #.gallery auth handler

--- mysite/routes/auth.js
+++ mysite/routes/auth.js
@@ -1,20 +1,23 @@
 // 원래는 next가 있음. express가 알아서 error를 해줌. 그래서 생략 
 // 여기서는 해야됌 
-module.exports = function(req,res,next){
-    if(req.session.authUser){
-        // null이 아니면 다음꺼 실행해 ! -> controller.update 실행 
-        next();
-        return;
-    }
-    if(req.accepts('html')){
-       res.redirect("/user/login");
-        return;
-    }
+module.exports = function(role){
+    return function(req,res,next){
+        // Logic -> ADMIN check 
+        if(req.session.authUser && (role !== 'ADMIN' || req.session.authUser.role === 'ADMIN')){
+            next();
+            return;
+        }
 
-    // JSON 요청임 
-    res.send({
-        result: "fail",
-        data: null,
-        message: "Auth failled"
-    })
+        if(req.accepts('html')){
+            res.redirect(req.session.authUser ? '/' : '/user/login');
+            return;
+        }
+
+        // JSON 요청임 
+        res.send({
+            result: "fail",
+            data: null,
+            message: "Auth failled"
+        })
+    }
 }
--- mysite/routes/gallery.js
+++ mysite/routes/gallery.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const controller = require("../controllers/gallery");
-//const auth = require('./auth');
+const authorized = require('./auth');
 
 const router = express.Router();
 
 router.route("").get(controller.index);
-router.route("/upload").post(controller.upload);
-router.route("/delete").get(controller.delete);
+router.route("/upload").post(authorized('ADMIN'), controller.upload);
+router.route("/delete/:no").get(authorized('ADMIN'), controller.delete);
 
 
 module.exports = router;
