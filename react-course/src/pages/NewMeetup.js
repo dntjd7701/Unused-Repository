4f3f7cd48d6b3812dd20531127905704eb5368ed
w <>
2023-06-19 23:29:13 +0900

    no message

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,5 +1,25 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 const NewMeetup = () => {
-  return <div>NewMeetup</div>;
+  const onAddMeetupHandler = (data) => {
+    fetch(
+      'https://react-course-b5931-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }
+    );
+  };
+
+  return (
+    <section>
+      <h1>New Meetup</h1>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetup;
