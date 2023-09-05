99d8a3ee37a5e72340608fe42f725f5dcc258472
w <>
2023-09-05 22:21:08 +0900

    동적 라우트로 내비게이팅하기

---
+++ nextjs-course/pages/portfolio/[projectId].js
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router';
+
+const PortFolioProjectPage = () => {
+  const router = useRouter();
+  return <h1>The PortFolioProjectPage !</h1>;
+};
+
+export default PortFolioProjectPage;
---
+++ nextjs-course/pages/portfolio/index.js
@@ -0,0 +1,5 @@
+function PortFolioPage() {
+  return <h1>This is PortFolioPage</h1>;
+}
+
+export default PortFolioPage;
