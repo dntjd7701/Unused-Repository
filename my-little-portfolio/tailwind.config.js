7d04446a44e3a2a1db030196d7f19c9dcd5ef0b3
강우성 <peacegrace7701@douzone.com>
2023-11-01 19:56:06 +0900

    my-little-portfolio nav ing

--- my-little-portfolio/tailwind.config.js
+++ my-little-portfolio/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00040f",
+      }
+    },
+    // 반응형 작업을 위한 스크린 사이즈 정의 
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
