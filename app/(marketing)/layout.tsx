6b55f3993b6e7576475543fc0b3b45719d2a1ed7
w <>
2024-02-27 22:59:07 +0900

    font 적용

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,12 @@
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full bg-slate-100">
-      {/* 네비게이션 바 */}
-      <main className="pt-40 pb-20">{children}</main>
-      {/* Footer */}
-    </div>
-  );
+   return (
+      <div className="h-full bg-slate-100">
+         {/* Nav bar */}
+         <main className="pt-40 pb-20">{children}</main>
+
+         {/* Footer */}
+      </div>
+   );
 };
 
 export default MarketingLayout;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,14 +1,31 @@
 // npm
 import { Medal } from 'lucide-react';
+import localFont from 'next/font/local';
+import { Poppins } from 'next/font/google';
 import Link from 'next/link';
 
 // shadcn-ui
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const headingFont = localFont({
+   src: '../../public/fonts/font.woff2',
+});
+
+const PoppinFont = Poppins({
+   subsets: ['latin'],
+   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 const MarketingPage = () => {
    return (
       <div className="flex items-center justify-center flex-col">
-         <div className="flex items-center justify-center flex-col">
+         <div
+            className={cn(
+               'flex items-center justify-center flex-col',
+               headingFont.className,
+            )}
+         >
             <div className="mb-4 flex justify-center items-center border shadow-sm p-6 bg-amber-100 text-amber-700 uppercase rounded-full">
                <Medal className="w-8 h-6 m-2" />
                <span className="text-md px-3">
@@ -22,9 +39,13 @@ const MarketingPage = () => {
                work forward
             </div>
          </div>
-         <div className="text-xs md:text-xl text-neutral-400 mt-7 max-w-md md:max-w-2xl text-center mx-auto">
-            협력하고, 프로젝트를 관리하고, 생산성을 높이세요. 어쩌고 저쩌고.
-            광고성 글을 막 !
+         <div
+            className={cn(
+               'text-xs md:text-xl text-neutral-400 mt-7 max-w-md md:max-w-2xl text-center mx-auto',
+               PoppinFont.className,
+            )}
+         >
+            Collaborate
          </div>
          <Button className="mt-6" size={'lg'} asChild>
             <Link href={'/sign-up'}>
